08d485d3885b6ccc971e6d49137ee9c3762e14ae
Grigoriy Popov <grisha_mgn@bk.ru>
2024-03-16 19:48:07 +0300

    all matches to upper case

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-const sectionText = document.querySelector(".section__text");
-
-const range = new Range();
-
-const startOffset = 0; // начало выделения
-const endOffset = Math.round(sectionText.textContent.length / 2); // конец выделения (половина текста)
-range.setStart(sectionText, startOffset);
-range.setEnd(sectionText, endOffset);
-
-// Выводим выделенный текст в консоль (для демонстрации)
-console.log(range.toString());
+const body = document.body;
+const searchArea = body.querySelector(".main");
 
 
-// const highlight = new Highlight(range1);
- 
-// CSS.highlights.set("first-highlight", highlight);
+const search = document.getElementById("search");
+
+search.addEventListener("input", () => {
+    console.log(search.value);
+})
+
+const treeWalker = document.createTreeWalker(searchArea, NodeFilter.SHOW_TEXT);
+
+const textObjects = [];
+while (treeWalker.nextNode()) {
+    const currNode = treeWalker.currentNode;
+    if (currNode.data.trim()) textObjects.push(currNode);
+}
+
+console.log(textObjects)
+
+textObjects.forEach(textNode => {
+    textNode.data = textNode.data.replaceAll(search.value, search.value.toUpperCase());
+})
+
+
+
+
