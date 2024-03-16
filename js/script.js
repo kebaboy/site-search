a2cbdbdd9227978a013a21f914337cef5089bd8e
Grigoriy Popov <grisha_mgn@bk.ru>
2024-03-16 21:51:58 +0300

    add raw hightligh algorythm

--- js/script.js
+++ js/script.js
@@ -2,26 +2,38 @@ const body = document.body;
 const searchArea = body.querySelector(".main");
 
 
-const search = document.getElementById("search");
-
-search.addEventListener("input", () => {
-    console.log(search.value);
-})
-
 const treeWalker = document.createTreeWalker(searchArea, NodeFilter.SHOW_TEXT);
 
 const textObjects = [];
 while (treeWalker.nextNode()) {
     const currNode = treeWalker.currentNode;
-    if (currNode.data.trim()) textObjects.push(currNode);
+    if (currNode.textContent.trim()) textObjects.push(currNode);
 }
 
-console.log(textObjects)
+const search = document.getElementById("search");
 
-textObjects.forEach(textNode => {
-    textNode.data = textNode.data.replaceAll(search.value, search.value.toUpperCase());
+search.addEventListener("input", () => {
+    CSS.highlights.clear();
+    if ( !search.value.trim() ) return;
+
+    const ranges = textObjects.map(el => {
+        let indices = [];
+        let i = 0;
+        while (el.textContent.indexOf(search.value, i) !== -1) {
+            indices.push(el.textContent.indexOf(search.value, i));
+            i = el.textContent.indexOf(search.value, i) + search.value.length;
+        }
+        return indices.map(index => {
+            const range = new Range();
+            range.setStart(el, index);
+            range.setEnd(el, index + search.value.length);
+            return range;
+        });
+    })
+    
+    const searchResultsHighlight = new Highlight(...ranges.flat());
+    
+    CSS.highlights.set("search-results", searchResultsHighlight);
 })
-
-
 
 
