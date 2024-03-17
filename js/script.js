8189c6e216aeeb669b33ee2f4b32767db36fb9f5
Grigoriy Popov <grisha_mgn@bk.ru>
2024-03-17 11:56:35 +0300

    add results count and add some function fixes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const body = document.body;
 const searchArea = body.querySelector(".main");
-
+const search = document.getElementById("search");
+const searchMatches = document.getElementById("results-count");
 
 const treeWalker = document.createTreeWalker(searchArea, NodeFilter.SHOW_TEXT);
 
@@ -10,30 +11,47 @@ while (treeWalker.nextNode()) {
     if (currNode.textContent.trim()) textObjects.push(currNode);
 }
 
-const search = document.getElementById("search");
 
-search.addEventListener("input", () => {
+search.addEventListener("input", function() {
+
     CSS.highlights.clear();
-    if ( !search.value.trim() ) return;
+
+    const searchInput = search.value.trim();
+    if ( !searchInput ) {
+        searchMatches.parentElement.classList.remove("display");
+        return;
+    }
 
     const ranges = textObjects.map(el => {
-        let indices = [];
+
+        const indices = [];
+        const elTextContent = el.textContent.trim();
         let i = 0;
-        while (el.textContent.indexOf(search.value, i) !== -1) {
-            indices.push(el.textContent.indexOf(search.value, i));
-            i = el.textContent.indexOf(search.value, i) + search.value.length;
+        let index = elTextContent.indexOf(searchInput, i);
+
+        while (index !== -1) {
+            indices.push(index);
+            i = index + searchInput.length;
+            index = elTextContent.indexOf(searchInput, i);
         }
+
         return indices.map(index => {
             const range = new Range();
             range.setStart(el, index);
-            range.setEnd(el, index + search.value.length);
+            range.setEnd(el, index + searchInput.length);
             return range;
         });
+
     })
     
-    const searchResultsHighlight = new Highlight(...ranges.flat());
-    
-    CSS.highlights.set("search-results", searchResultsHighlight);
+    if (ranges.flat().length !== 0) {
+        const searchResultsHighlight = new Highlight(...ranges.flat());
+        CSS.highlights.set("search-results", searchResultsHighlight);
+    }
+
+    searchMatches.parentElement.classList.add("display");
+    searchMatches.textContent = ranges.flat().length;
+
 })
 
 
